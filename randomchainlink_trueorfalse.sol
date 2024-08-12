4020d570c81ee4d857529b35e4ef25f59c70e789
Tiến Đạt Magic <54920578+tiendatmagic@users.noreply.github.com>
2024-08-12 15:48:54 +0700

    Update randomchainlink_trueorfalse.sol

--- randomchainlink_trueorfalse.sol
+++ randomchainlink_trueorfalse.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.26;
 
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts@1.2.0/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 import {VRFV2PlusClient} from "@chainlink/contracts@1.2.0/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
