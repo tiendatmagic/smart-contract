0d6a464815c6f4e086d9484bf18f733859f0e9a8
Tiến Đạt Magic <54920578+tiendatmagic@users.noreply.github.com>
2024-08-18 01:08:49 +0700

    update smart contract

--- coin-flip-chainlink.sol
+++ coin-flip-chainlink.sol
@@ -7,8 +7,7 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract CoinFlipGame is VRFConsumerBaseV2Plus {
     event RequestSent(uint256 requestId, uint32 numWords);
-    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
-    event CoinFlipResult(uint256 requestId, uint256 result);
+    event RequestFulfilled(uint256 requestId, uint256 result);
     event BetPlaced(
         uint256 requestId,
         address bettor,
@@ -31,16 +30,13 @@ contract CoinFlipGame is VRFConsumerBaseV2Plus {
 
     struct RequestStatus {
         bool fulfilled;
-        bool exists;
         uint256 result;
         Bet bet;
     }
+
     mapping(uint256 => RequestStatus) public s_requests;
 
     uint256 public s_subscriptionId;
-    uint256[] public requestIds;
-    uint256 public lastRequestId;
-
     uint8 public houseFeePercentage; // Fee percentage (0-100)
     uint256 public maxBetAmount;
     uint256 public minBetAmount;
@@ -49,7 +45,6 @@ contract CoinFlipGame is VRFConsumerBaseV2Plus {
 
     bytes32 public keyHash =
         0x8596b430971ac45bdf6088665b9ad8e8630c9d5049ab54b14dff711bee7c0e26;
-
     uint32 public callbackGasLimit = 300000;
     uint16 public requestConfirmations = 3;
     uint32 public numWords = 1; // Only need one word
@@ -63,33 +58,26 @@ contract CoinFlipGame is VRFConsumerBaseV2Plus {
     }
 
     function placeBet(
-        uint256 betAmount, // Amount of the bet
-        uint8 choice, // Choice of 0 or 1
-        address referral // Referral address
+        uint256 betAmount,
+        uint8 choice,
+        address referral
     ) external returns (uint256 requestId) {
         require(
-            betAmount >= minBetAmount,
-            "Bet amount must be greater than or equal to the minimum bet amount"
+            betAmount >= minBetAmount && betAmount <= maxBetAmount,
+            "Bet amount out of range"
         );
 
-        require(
-            betAmount <= maxBetAmount,
-            "Bet amount exceeds the maximum limit"
-        );
-
-        uint256 potentialPayout = (betAmount * 2);
+        uint256 potentialPayout = betAmount * 2;
         require(
             bettingToken.balanceOf(address(this)) >= potentialPayout,
             "Insufficient contract balance"
         );
 
-        // Transfer tokens from the bettor to the contract
         require(
             bettingToken.transferFrom(msg.sender, address(this), betAmount),
             "Token transfer failed"
         );
 
-        // Request random words from Chainlink VRF
         requestId = s_vrfCoordinator.requestRandomWords(
             VRFV2PlusClient.RandomWordsRequest({
                 keyHash: keyHash,
@@ -105,7 +93,6 @@ contract CoinFlipGame is VRFConsumerBaseV2Plus {
 
         s_requests[requestId] = RequestStatus({
             result: 0,
-            exists: true,
             fulfilled: false,
             bet: Bet({
                 bettor: msg.sender,
@@ -114,8 +101,7 @@ contract CoinFlipGame is VRFConsumerBaseV2Plus {
                 referral: referral
             })
         });
-        requestIds.push(requestId);
-        lastRequestId = requestId;
+
         emit RequestSent(requestId, numWords);
         emit BetPlaced(requestId, msg.sender, betAmount, choice, referral);
         return requestId;
@@ -125,18 +111,15 @@ contract CoinFlipGame is VRFConsumerBaseV2Plus {
         uint256 _requestId,
         uint256[] calldata _randomWords
     ) internal override {
-        require(s_requests[_requestId].exists, "request not found");
+        RequestStatus storage request = s_requests[_requestId];
+        require(request.fulfilled == false, "Request already fulfilled");
 
-        // Determine the result
-        uint256 result = _randomWords[0] % 2; // Map to 0 or 1
-        s_requests[_requestId].fulfilled = true;
-        s_requests[_requestId].result = result;
+        request.result = _randomWords[0] % 2; // Map to 0 or 1
+        request.fulfilled = true;
 
-        // Handle bet result
-        handleBetResult(_requestId, result);
+        handleBetResult(_requestId, request.result);
 
-        emit RequestFulfilled(_requestId, _randomWords);
-        emit CoinFlipResult(_requestId, result);
+        emit RequestFulfilled(_requestId, request.result);
     }
 
     function handleBetResult(uint256 requestId, uint256 result) internal {
@@ -172,10 +155,7 @@ contract CoinFlipGame is VRFConsumerBaseV2Plus {
     }
 
     function setHouseFee(uint8 _feePercentage) external onlyOwner {
-        require(
-            _feePercentage >= 0 && _feePercentage <= 100,
-            "Invalid fee percentage"
-        );
+        require(_feePercentage <= 100, "Invalid fee percentage");
         houseFeePercentage = _feePercentage;
         emit HouseFeeUpdated(_feePercentage);
     }
@@ -195,10 +175,7 @@ contract CoinFlipGame is VRFConsumerBaseV2Plus {
     function setReferralBonusPercentage(
         uint16 _bonusPercentage
     ) external onlyOwner {
-        require(
-            _bonusPercentage >= 0 && _bonusPercentage <= 100,
-            "Invalid bonus percentage"
-        );
+        require(_bonusPercentage <= 1000, "Invalid bonus percentage");
         referralBonusPercentage = _bonusPercentage;
         emit ReferralBonusUpdated(_bonusPercentage);
     }
@@ -210,17 +187,13 @@ contract CoinFlipGame is VRFConsumerBaseV2Plus {
     function getRequestStatus(
         uint256 _requestId
     ) external view returns (bool fulfilled, uint256 result) {
-        require(s_requests[_requestId].exists, "request not found");
         RequestStatus memory request = s_requests[_requestId];
         return (request.fulfilled, request.result);
     }
 
-    // Function to withdraw balance
     function withdrawBalance(uint256 amount) external onlyOwner {
         uint256 balance = bettingToken.balanceOf(address(this));
-        require(amount > 0, "Amount must be greater than 0");
-        require(amount <= balance, "Insufficient balance in the contract");
-
+        require(amount > 0 && amount <= balance, "Invalid amount");
         require(bettingToken.transfer(msg.sender, amount), "Withdrawal failed");
         emit BalanceWithdrawn(msg.sender, amount);
     }
