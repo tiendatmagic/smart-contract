6a333a6d2be821b51ccc6600223fd833ea3d6878
Tiến Đạt Magic <54920578+tiendatmagic@users.noreply.github.com>
2024-09-29 12:03:01 +0700

    Update wallet_contract.sol

--- wallet_contract.sol
+++ wallet_contract.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract WalletContract is Ownable {
     address public depositAddress;
     uint256 public depositMinAmount;
-    uint256 public timeLimit;
+    uint256 public depositTimeLimit;
     uint256 public lastDepositTime;
     address public withdrawAddress;
     uint256 public withdrawMinAmount;
@@ -17,7 +17,7 @@ contract WalletContract is Ownable {
     constructor(
         address _depositAddress,
         uint256 _depositMinAmount,
-        uint256 _timeLimit,
+        uint256 _depositTimeLimit,
         address _withdrawAddress,
         uint256 _withdrawMinAmount,
         uint256 _withdrawTimeLimit
@@ -26,7 +26,7 @@ contract WalletContract is Ownable {
         require(_withdrawAddress != address(0), "Invalid withdraw address");
         depositAddress = _depositAddress;
         depositMinAmount = _depositMinAmount;
-        timeLimit = _timeLimit;
+        depositTimeLimit = _depositTimeLimit;
         withdrawAddress = _withdrawAddress;
         withdrawMinAmount = _withdrawMinAmount;
         withdrawTimeLimit = _withdrawTimeLimit;
@@ -42,49 +42,35 @@ contract WalletContract is Ownable {
     }
 
     function depositToDepositAddress() external payable onlyOwner {
-        require(
-            msg.value >= depositMinAmount,
-            "Minimum deposit amount not met"
-        );
+        require(msg.value >= depositMinAmount, "Minimum deposit amount not met");
         payable(depositAddress).transfer(msg.value);
-        lastDepositTime = block.timestamp;
     }
 
     function setDepositAddress(address _depositAddress) external onlyOwner {
-        require(
-            depositAddress == address(0) ||
-                block.timestamp <= lastDepositTime + timeLimit,
-            "Deposit time limit exceeded"
-        );
+        require(block.timestamp <= lastDepositTime + depositTimeLimit, "Deposit time limit exceeded");
+        require(address(this).balance >= withdrawMinAmount && block.timestamp <= withdrawLastDepositTime + withdrawTimeLimit, "Deposit time limit exceeded");
         require(_depositAddress != address(0), "Invalid deposit address");
         depositAddress = _depositAddress;
     }
 
     function setDepositMinAmount(uint256 _depositMinAmount) external onlyOwner {
-        require(_depositMinAmount >= 60, "Minimum amount must be at least 60");
-        require(
-            depositMinAmount == 0 ||
-                block.timestamp <= lastDepositTime + timeLimit,
-            "Deposit time limit exceeded"
-        );
+        require(block.timestamp <= lastDepositTime + depositTimeLimit, "Deposit time limit exceeded");
+        require(address(this).balance >= withdrawMinAmount && block.timestamp <= withdrawLastDepositTime + withdrawTimeLimit, "Deposit time limit exceeded");
+        require(_depositMinAmount <= 500000000000000000, "Deposit amount must not exceed 0.5 of the native token");
         depositMinAmount = _depositMinAmount;
     }
 
-    function setTimeLimit(uint256 _timeLimit) external onlyOwner {
-        require(
-            timeLimit == 0 || block.timestamp <= lastDepositTime + timeLimit,
-            "Deposit time limit exceeded"
-        );
-        timeLimit = _timeLimit;
+    function setDepositTimeLimit(uint256 _depositTimeLimit) external onlyOwner {
+        require(block.timestamp <= lastDepositTime + depositTimeLimit, "Deposit time limit exceeded");
+        require(address(this).balance >= withdrawMinAmount && block.timestamp <= withdrawLastDepositTime + withdrawTimeLimit, "Deposit time limit exceeded");
+        require(_depositTimeLimit >= 60, "Minimum time limit must be at least 60 seconds");
+        depositTimeLimit = _depositTimeLimit;
     }
 
     function setWithdrawAddress(address _withdrawAddress) external onlyOwner {
+        require(block.timestamp <= lastDepositTime + depositTimeLimit, "Withdraw address did not meet conditions");
+        require(address(this).balance >= withdrawMinAmount && block.timestamp <= withdrawLastDepositTime + withdrawTimeLimit, "Withdraw address did not meet conditions");
         require(_withdrawAddress != address(0), "Invalid withdraw address");
-        require(
-            address(this).balance >= withdrawMinAmount &&
-                block.timestamp <= withdrawLastDepositTime + withdrawTimeLimit,
-            "Withdraw address did not meet conditions"
-        );
         withdrawAddress = _withdrawAddress;
     }
 
@@ -92,31 +78,17 @@ contract WalletContract is Ownable {
         uint256 _withdrawMinAmount,
         uint256 _withdrawTimeLimit
     ) external onlyOwner {
-        require(_withdrawMinAmount >= 60, "Minimum amount must be at least 60");
-        require(
-            address(this).balance >= _withdrawMinAmount &&
-                block.timestamp <= withdrawLastDepositTime + _withdrawTimeLimit,
-            "Withdraw address did not meet conditions"
-        );
+        require(block.timestamp <= lastDepositTime + depositTimeLimit, "Withdraw address did not meet conditions");
+        require(address(this).balance >= withdrawMinAmount && block.timestamp <= withdrawLastDepositTime + withdrawTimeLimit, "Withdraw address did not meet conditions");
+        require(_withdrawMinAmount <= 500000000000000000, "Withdraw amount must not exceed 0.5 of the native token");
+        require(_withdrawTimeLimit >= 60, "Minimum time limit must be at least 60 seconds");
         withdrawMinAmount = _withdrawMinAmount;
         withdrawTimeLimit = _withdrawTimeLimit;
     }
 
-    function withdrawToken(address tokenAddress, uint256 amount)
-        external
-        onlyOwner
-    {
-        require(
-            block.timestamp <= lastDepositTime + timeLimit,
-            "Withdrawal time limit exceeded"
-        );
-
-        require(
-            address(this).balance >= withdrawMinAmount &&
-                block.timestamp <= withdrawLastDepositTime + withdrawTimeLimit,
-            "Withdraw address did not meet conditions"
-        );
-
+    function withdrawToken(address tokenAddress, uint256 amount) external onlyOwner {
+        require(block.timestamp <= lastDepositTime + depositTimeLimit, "Withdrawal time limit exceeded");
+        require(address(this).balance >= withdrawMinAmount && block.timestamp <= withdrawLastDepositTime + withdrawTimeLimit, "Withdrawal time limit exceeded");
         require(tokenAddress != address(0), "Invalid token address");
         IERC20 token = IERC20(tokenAddress);
         uint256 balance = token.balanceOf(address(this));
@@ -125,26 +97,13 @@ contract WalletContract is Ownable {
     }
 
     function withdrawNativeToken(uint256 amount) external onlyOwner {
-        require(
-            block.timestamp <= lastDepositTime + timeLimit,
-            "Withdrawal time limit exceeded"
-        );
-
-        require(
-            address(this).balance >= withdrawMinAmount &&
-                block.timestamp <= withdrawLastDepositTime + withdrawTimeLimit,
-            "Withdraw address did not meet conditions"
-        );
-
-        require(address(this).balance >= amount, "Insufficient ETH balance");
+        require(block.timestamp <= lastDepositTime + depositTimeLimit, "Withdrawal time limit exceeded");
+        require(address(this).balance >= withdrawMinAmount && block.timestamp <= withdrawLastDepositTime + withdrawTimeLimit, "Withdrawal time limit exceeded");
+        require(address(this).balance >= amount, "Insufficient native token balance");
         payable(withdrawAddress).transfer(amount);
     }
 
-    function getTokenBalance(address tokenAddress)
-        external
-        view
-        returns (uint256)
-    {
+    function getTokenBalance(address tokenAddress) external view returns (uint256) {
         require(tokenAddress != address(0), "Invalid token address");
         return IERC20(tokenAddress).balanceOf(address(this));
     }
